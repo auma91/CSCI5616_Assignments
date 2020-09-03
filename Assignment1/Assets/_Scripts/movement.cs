b66ac12cef04226c53ab4eeb91ae301a40982147
Aurangzeb Malik <auma9171@colorado.edu>
2020-09-03 13:47:24 -0600

    first commit

---
+++ Assignment1/Assets/_Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 position;
+    public float moveSpeed;
+    public Vector3 scale = Vector3.one;
+    public Vector3 rotation;
+    float counter = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        this.transform.Translate(position * Time.deltaTime);
+        this.transform.Rotate(rotation * Time.deltaTime);
+        print(Time.deltaTime);
+        print(counter);
+    }
+}
